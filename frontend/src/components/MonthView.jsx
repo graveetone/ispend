2f7f696ff016baf63dc5fd3d370220164c90e229
zasviat <zasviat@gmail.com>
2025-11-22 22:28:59 +0200

    add month view

--- frontend/src/components/MonthView.jsx
+++ frontend/src/components/MonthView.jsx
@@ -1,94 +1,119 @@
-import { useState, useEffect } from 'react'
-import axios from "axios"
-import Table from "./Table"
-// import './App.css'
+import { useState, useEffect } from "react";
+import dayjs from "dayjs";
+import {getMonthTransactions} from "./../api"
 
-const bordered = {
-    border: "white 1px solid"
-}
-  const containerStyle = {
-    display: "flex",
-    justifyContent: "space-around",
-    border: "1px red solid",
-//     border: "white 1px solid"
-
-    padding: "20px",
-//     gap: "10px",
-//     alignItems: "center",
-  };
-
-const getCurrentMonthDate = () => {
-    const today = new Date();
-    return new Date(today.getFullYear(), today.getMonth(), 1)
-}
-
-function MonthView() {
-  // keep current date in state
-  const [currentDate, setCurrentDate] = useState(getCurrentMonthDate());
-  const [response, setResponse] = useState({});
-
-  // go to previous month
-  const handlePrev = () => {
-    const prev = new Date(currentDate);
-    prev.setMonth(currentDate.getMonth() - 1);
-    setCurrentDate(prev);
-  };
-
-  // go to next month
-  const handleNext = () => {
-    const next = new Date(currentDate);
-    next.setMonth(currentDate.getMonth() + 1);
-    setCurrentDate(next);
-  };
-
-  // format month and year
-  const monthString = currentDate.toLocaleDateString("en-US", {
-    month: "long",
-    year: "numeric",
+export default function MonthView({transactions}) {
+  const [month, setMonth] = useState(dayjs().startOf("month"));
+  const [data, setData] = useState({
+    expenses: [],
+    incomes: [],
   });
 
   useEffect(() => {
-      const date = currentDate.toLocaleDateString("en-CA", {
-        day: "numeric",
-        month: "numeric",
-        year: "numeric",
-      });
-    console.log(`Request to http://0.0.0.0:8000/api/v1/months/${date}`)
-      axios.get(`http://0.0.0.0:8000/api/v1/months/${date}`)
-        .then(response => {
-            console.log(response.data)
-            setResponse(response.data)
-          })
-          .catch(error => {
-            console.error("Error fetching data:", error);
-          });
-  }, [currentDate])
+    const loadMonth = async () => {
+      const monthStr = month.format("YYYY-MM-DD");
+      const data = await getMonthTransactions(monthStr)
 
+      setData(data);
+    };
+
+    loadMonth();
+  }, [month, transactions]);
+
+
+  const prevMonth = () => {
+    setMonth(month.subtract(1, "month"));
+  };
+
+  const nextMonth = () => {
+    setMonth(month.add(1, "month"));
+  };
 
   return (
-      <>
-        <div className="flex items-center gap-4">
-          <button
-            onClick={handlePrev}
-            className="px-2 py-1 bg-gray-200 rounded hover:bg-gray-300"
-          >
-            &lt;
-          </button>
-          <span className="text-lg font-medium">{monthString}</span>
-          <button
-            onClick={handleNext}
-            className="px-2 py-1 bg-gray-200 rounded hover:bg-gray-300"
-          >
-            &gt;
-          </button>
-        </div>
-        <div style={containerStyle}>
-        </div>
-        <Table title="Expenses" data={response.expenses} />
-        <Table title="Incomes" data={response.incomes} />
-    </>
+    <div className="p-6 w-full flex flex-col items-around">
+
+      {/* Header */}
+      <div className="flex items-center justify-between mb-6">
+        <button
+          onClick={prevMonth}
+          className="px-3 py-2 bg-black text-white hover:text-white rounded-lg hover:bg-red-500 border border-2 border-red-500"
+        >
+          ← Prev
+        </button>
+
+        <h1 className="text-2xl font-semibold">
+          {month.format("MMMM YYYY")}
+        </h1>
+
+        <button
+          onClick={nextMonth}
+          className="px-3 py-2 bg-black text-white hover:text-white rounded-lg hover:bg-red-500 border border-2 border-red-500"
+        >
+          Next →
+        </button>
+      </div>
+
+      {/* Expenses */}
+      <div className="mb-10">
+        <h2 className="text-xl font-semibold mb-3">Expenses</h2>
+        <table className="w-full text-left">
+          <thead className="bg-red-500">
+            <tr>
+              <th className="border p-4">Category</th>
+              <th className="border p-4">Amount</th>
+              <th className="border p-4">Planned</th>
+            </tr>
+          </thead>
+          <tbody className="">
+            {data.expenses.map((item, idx) => (
+              <tr key={idx} className="">
+                <td className="border p-2">{item.category}</td>
+                <td className="border p-2">{item.actual?.toFixed(2)}</td>
+                <td className="border p-2">{item.planned?.toFixed(2)}</td>
+              </tr>
+            ))}
+
+            {data.expenses.length === 0 && (
+              <tr>
+                <td className="p-3 text-center text-slate-500" colSpan={3}>
+                  No data
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+
+      {/* Incomes */}
+      <div>
+        <h2 className="text-xl font-semibold mb-3">Incomes</h2>
+        <table className="w-full text-left">
+          <thead className="bg-red-500">
+            <tr>
+              <th className="border p-2">Category</th>
+              <th className="border p-2">Amount</th>
+              <th className="border p-2">Planned</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.incomes.map((item, idx) => (
+              <tr key={idx} className="">
+                <td className="border p-2">{item.category}</td>
+                <td className="border p-2">{item.actual?.toFixed(2)}</td>
+                <td className="border p-2">{item.planned?.toFixed(2)}</td>
+              </tr>
+            ))}
+
+            {data.incomes.length === 0 && (
+              <tr>
+                <td className="p-3 text-center text-slate-500" colSpan={3}>
+                  No data
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
-
-
-export default MonthView;
