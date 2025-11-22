d9a09c392e551a1d723ae7157ad983148804d530
zasviat <zasviat@gmail.com>
2025-11-22 22:28:24 +0200

    setup navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard'
 import MonthCalendar from './components/Calendar'
 
 export default function App() {
   return (
     <>
-    {/* Navigation */}
-    <nav>
-      {/* <Link to="/">Dashboard</Link>
-      <Link to="/calendar">Calendar</Link> */}
-      {/* <Link to="/contact">Contact</Link> */}
-    </nav>
-
-    {/* Routes */}
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/calendar" element={<MonthCalendar />} />
-      {/* <Route path="/contact" element={<Contact />} /> */}
     </Routes>
 
   </>
