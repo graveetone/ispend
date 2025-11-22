5b1005e5a4fc62edeaf0688c99854f1e683053b1
zasviat <zasviat@gmail.com>
2025-11-22 22:24:24 +0200

    setup frontend linter and tailwind configs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MainPage from "./components/MainPage"
+import React from 'react'
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import Dashboard from './components/Dashboard'
+import MonthCalendar from './components/Calendar'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-        <MainPage />
-{/*       <div> */}
-{/*         <a href="https://vite.dev" target="_blank"> */}
-{/*           <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-{/*         </a> */}
-{/*         <a href="https://react.dev" target="_blank"> */}
-{/*           <img src={reactLogo} className="logo react" alt="React logo" /> */}
-{/*         </a> */}
-{/*       </div> */}
-{/*       <h1>Vite + React</h1> */}
-{/*       <div className="card"> */}
-{/*         <button onClick={() => setCount((count) => count + 1)}> */}
-{/*           count is {count} */}
-{/*         </button> */}
-{/*         <p> */}
-{/*           Edit <code>src/App.jsx</code> and save to test HMR */}
-{/*         </p> */}
-{/*       </div> */}
-{/*       <p className="read-the-docs"> */}
-{/*         Click on the Vite and React logos to learn more */}
-{/*       </p> */}
-    </>
+    {/* Navigation */}
+    <nav>
+      {/* <Link to="/">Dashboard</Link>
+      <Link to="/calendar">Calendar</Link> */}
+      {/* <Link to="/contact">Contact</Link> */}
+    </nav>
+
+    {/* Routes */}
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/calendar" element={<MonthCalendar />} />
+      {/* <Route path="/contact" element={<Contact />} /> */}
+    </Routes>
+
+  </>
   )
 }
-
-export default App
